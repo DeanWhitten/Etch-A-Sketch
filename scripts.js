ebff107fcbc3ed0e101cac07a21daa133b3dce31
DEAN-Ubuntu <dean.f.whitten@gmail.com>
2020-05-05 14:05:46 -0500

    Seperated files and made responsive

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+document.getElementById("mainContainer").style.cssText = "height: 100vh; width: 100vw; background-color: coral;"
+
+const container = document.getElementById("sketchBox");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+
+    cell.addEventListener("mouseover", paint);
+    cell.addEventListener("click", erase);
+    document.getElementById("clear").addEventListener("click", erase);
+
+    container.appendChild(cell).className = "grid-item";
+
+    function paint (){
+        cell.style.cssText = "background-color: blue;"
+    };
+
+    function erase (){
+       cell.style.cssText = "background-color: blanchedalmond;"
+
+       
+
+    };
+
+  };
+};
+
+makeRows(16, 16);
